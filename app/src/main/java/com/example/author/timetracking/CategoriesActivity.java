408f4d9c32d0b39f9d47553761aa281a8d8b9eed
AuthorSkN <author.skn@gmail.com>
2019-01-08 13:55:42 +0300

    navigation in menu2

---
+++ app/src/main/java/com/example/author/timetracking/CategoriesActivity.java
@@ -0,0 +1,15 @@
+package com.example.author.timetracking;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CategoriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+
+}
---
+++ app/src/main/java/com/example/author/timetracking/StatisticActivity.java
@@ -0,0 +1,29 @@
+package com.example.author.timetracking;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class StatisticActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic);
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+    }
+
+}
